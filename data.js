9da98f5dcc048dc757e3908bf68a59a6eef92cc0
Sanju Kamat <sanjukamat123@gmail.com>
2023-07-03 19:33:44 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,833 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pool-deck-",
+      "name": "Pool Deck ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.14156945026116396,
+        "pitch": -0.030201892074387615,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.21476540291009627,
+          "pitch": 0.06800606954739408,
+          "rotation": 0,
+          "target": "1-pool-area-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pool-area-1",
+      "name": "Pool Area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.41432511758695156,
+        "pitch": 0.2953554203080291,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0135355580396155,
+          "pitch": 0.035877783598197155,
+          "rotation": 0,
+          "target": "0-pool-deck-"
+        },
+        {
+          "yaw": 1.1063579757328412,
+          "pitch": 0.027187863854813088,
+          "rotation": 0,
+          "target": "5-dining-space"
+        },
+        {
+          "yaw": 0.19747919952823878,
+          "pitch": 0.042358302556362304,
+          "rotation": 0,
+          "target": "2-pool-area-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pool-area-2",
+      "name": "Pool Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.4086295216658016,
+        "pitch": 0.09229983527930408,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.184665950253695,
+          "pitch": -0.022635308084092642,
+          "rotation": 0,
+          "target": "0-pool-deck-"
+        },
+        {
+          "yaw": 2.0562780449264615,
+          "pitch": -0.06360392072794241,
+          "rotation": 0,
+          "target": "8-living-space"
+        },
+        {
+          "yaw": 1.2337088020852995,
+          "pitch": -0.1226762056800883,
+          "rotation": 1.5707963267948966,
+          "target": "16-pool-shower-area"
+        },
+        {
+          "yaw": -0.44008895078411925,
+          "pitch": 0.07266930339257449,
+          "rotation": 0,
+          "target": "3-pool-area-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pool-area-3",
+      "name": "Pool Area 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.37985603392140277,
+        "pitch": 0.028245799359840618,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.110501481835433,
+          "pitch": -0.08919754618304054,
+          "rotation": 0,
+          "target": "16-pool-shower-area"
+        },
+        {
+          "yaw": 0.20391877028333383,
+          "pitch": -0.017182863408034876,
+          "rotation": 0,
+          "target": "1-pool-area-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.9834406126964943,
+        "pitch": 0.10624396554769788,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1271166183340036,
+          "pitch": 0.07255180007912898,
+          "rotation": 1.5707963267948966,
+          "target": "6-dining-space"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dining-space",
+      "name": "Dining Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.8259665527405051,
+        "pitch": 0.10723575568258426,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2047064717894127,
+          "pitch": 0.05890805799157128,
+          "rotation": 0,
+          "target": "7-living-space-1"
+        },
+        {
+          "yaw": -2.7617757734006823,
+          "pitch": 0.07710069462127755,
+          "rotation": 4.71238898038469,
+          "target": "4-kitchen"
+        },
+        {
+          "yaw": -0.5493391441076394,
+          "pitch": 0.09108542978799505,
+          "rotation": 0,
+          "target": "1-pool-area-1"
+        },
+        {
+          "yaw": -2.41945234260319,
+          "pitch": 0.057167296238777254,
+          "rotation": 0,
+          "target": "15-outdoor"
+        },
+        {
+          "yaw": 2.3999874084201505,
+          "pitch": -0.14059122399044455,
+          "rotation": 0,
+          "target": "12-stairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dining-space",
+      "name": "Dining Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.270629380688705,
+        "pitch": 0.1570692848290154,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.276933613676638,
+          "pitch": 0.01097531819036135,
+          "rotation": 0,
+          "target": "7-living-space-1"
+        },
+        {
+          "yaw": -2.7664368639988197,
+          "pitch": 0.07348473523910215,
+          "rotation": 4.71238898038469,
+          "target": "4-kitchen"
+        },
+        {
+          "yaw": -2.4295470211819143,
+          "pitch": 0.06127956356648667,
+          "rotation": 0,
+          "target": "15-outdoor"
+        },
+        {
+          "yaw": -0.4845994867108878,
+          "pitch": 0.10116980917675811,
+          "rotation": 0,
+          "target": "1-pool-area-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-living-space-1",
+      "name": "Living Space 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.05201473253086064,
+        "pitch": 0.026755228344420345,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.946646904321156,
+          "pitch": 0.028205255693816156,
+          "rotation": 0,
+          "target": "5-dining-space"
+        },
+        {
+          "yaw": 1.0212046869775975,
+          "pitch": 0.03916529055390683,
+          "rotation": 0,
+          "target": "8-living-space"
+        },
+        {
+          "yaw": 2.2132403010208854,
+          "pitch": 0.006712752359511853,
+          "rotation": 0,
+          "target": "17-bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-living-space",
+      "name": "Living Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.14002874720124048,
+        "pitch": 0.08370793221252804,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7842767952828105,
+          "pitch": 0.05971187136275091,
+          "rotation": 0,
+          "target": "9-living-space"
+        },
+        {
+          "yaw": 2.0378961754053284,
+          "pitch": 0.05774154950429633,
+          "rotation": 0,
+          "target": "10-living-space"
+        },
+        {
+          "yaw": -2.4129269389715464,
+          "pitch": 0.1184248952785456,
+          "rotation": 0,
+          "target": "2-pool-area-2"
+        },
+        {
+          "yaw": 1.4676721127970414,
+          "pitch": 0.10664838803534593,
+          "rotation": 0,
+          "target": "17-bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-living-space",
+      "name": "Living Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.7474034212317537,
+        "pitch": 0.11478140045200824,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.038163385151596785,
+          "pitch": 0.005607118639801456,
+          "rotation": 0,
+          "target": "13-common-washroom"
+        },
+        {
+          "yaw": 1.5100201889831322,
+          "pitch": 0.06879589231344241,
+          "rotation": 0,
+          "target": "11-living-space-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-living-space",
+      "name": "Living Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.305216829453114,
+        "pitch": 0.033184007347975,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9210668157388788,
+          "pitch": 0.041351039164780445,
+          "rotation": 0,
+          "target": "5-dining-space"
+        },
+        {
+          "yaw": 1.0151468551856802,
+          "pitch": 0.0197674464983546,
+          "rotation": 0,
+          "target": "8-living-space"
+        },
+        {
+          "yaw": 2.259190567821614,
+          "pitch": 0.053152652017708135,
+          "rotation": 0,
+          "target": "17-bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-living-space-",
+      "name": "LIving Space ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.118084990794424,
+        "pitch": -0.036246758037467686,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0083371164751798,
+          "pitch": 0.04712765987400935,
+          "rotation": 0,
+          "target": "10-living-space"
+        },
+        {
+          "yaw": -0.8076132156960423,
+          "pitch": 0.0978904519434689,
+          "rotation": 0,
+          "target": "9-living-space"
+        },
+        {
+          "yaw": 1.469400719653093,
+          "pitch": 0.08222144769917605,
+          "rotation": 0,
+          "target": "17-bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-stairs",
+      "name": "Stairs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.2518957575135836,
+        "pitch": 0.01510825509492264,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8506356533373243,
+          "pitch": 0.41390052285136214,
+          "rotation": 0,
+          "target": "6-dining-space"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-common-washroom",
+      "name": "Common Washroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.8910547751732487,
+        "pitch": 0.11780196362176021,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4139650608939931,
+          "pitch": 0.016092853029160636,
+          "rotation": 4.71238898038469,
+          "target": "11-living-space-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.4314328411657975,
+        "pitch": -0.02459525249200034,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3876982476141535,
+          "pitch": 0.04846912684538118,
+          "rotation": 0,
+          "target": "6-dining-space"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-outdoor",
+      "name": "Outdoor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.5089086398709988,
+        "pitch": -0.25929753750187423,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23226035205738071,
+          "pitch": -0.015773866497912792,
+          "rotation": 0,
+          "target": "14-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-pool-shower-area",
+      "name": "Pool Shower Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.8661987185255668,
+        "pitch": 0.25083705909803555,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.04251641485979,
+          "pitch": 0.11459602550577763,
+          "rotation": 4.71238898038469,
+          "target": "1-pool-area-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-bedroom-1",
+      "name": "Bedroom 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.329076772412451,
+        "pitch": 0.18745349849864468,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.185790239982765,
+          "pitch": 0.014191100639290966,
+          "rotation": 1.5707963267948966,
+          "target": "10-living-space"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Swimming Pool ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
